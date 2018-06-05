6cd809ae6f045bbaab640087f229733f64c7738e
Couper12 <coupersemail@gmail.com>
2018-06-05 10:19:28 -0400

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace u5_MathTutor_CouperEbbsPicken
     /// </summary>
     public partial class MainWindow : Window
     {
+        //variables
         int number1;
         int number2;
         int operation;
@@ -39,6 +40,7 @@ namespace u5_MathTutor_CouperEbbsPicken
 
         private void btnGenerate_Click(object sender, RoutedEventArgs e)
         {
+            // resets labels and variables
             lblOperation.Content = "What is ";
             lblOutpt.Content = "";
             answer = 0;
@@ -46,6 +48,7 @@ namespace u5_MathTutor_CouperEbbsPicken
             number2 = random.Next(1, 11);
             operation = random.Next(0, 4);
 
+            // checks to see what operation will be done 0 is multiply, 1 is addition, 2 is subtract, 3 is divide
             if (operation == 0)
             {
                 answer = number1 * number2;
@@ -77,8 +80,10 @@ namespace u5_MathTutor_CouperEbbsPicken
 
         private void btnCheckAnswer_Click(object sender, RoutedEventArgs e)
         {
+            // turns text into int
             int.TryParse(txtInput.Text, out int guess);
 
+            // checks if the guess is same as answer, and changes output label 
             if (guess == answer)
             {
                 lblOutpt.Content = "Correct!";
